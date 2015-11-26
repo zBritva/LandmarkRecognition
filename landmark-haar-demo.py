292296b8ef562aa7d2cb684a2ec9191e1ea1551b
Ильфат Галиев <zbritva@gmail.com>
2015-11-26 15:07:37 +0100

    haar demo

--- landmark-haar-demo.py
+++ landmark-haar-demo.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 __author__ = 'zBritva'
 
-
 import cv2
 import numpy as np
 
@@ -10,13 +9,19 @@ import numpy as np
 cap = cv2.VideoCapture(0)
 
 
-# image of land zone (H-mark, landmark)
-land_zone = cv2.imread('samples/land-zone-raspberry.jpg')
+# image data of leard (helipad, landmark)
+face_cascade = cv2.CascadeClassifier('train_data/train2/results/cascade.xml')
 
 while (True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
     frameOut = frame
 
