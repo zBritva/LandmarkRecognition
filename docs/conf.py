edc0c83eb4e8f3d7158645aa442491e9f80dd94a
Ильфат Галиев <zbritva@gmail.com>
2016-01-19 01:49:12 +0100

    conf.py changed

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,8 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('./..'))
-print 'CONFIGURATION ' + os.path.abspath('./..')
+sys.path.insert(0, os.path.abspath('./../lib'))
+import cv2
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
